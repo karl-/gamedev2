596d4f129692325aa86e90f5fd5724e04df31c72
karl <karlh07@gmail.com>
2016-08-25 14:39:25 -0400

    Add actual code from class 2 workshop

---
+++ class02/class_demo/Assets/Code/OnClick.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider))]
+public class OnClick : MonoBehaviour {
+
+	/**
+	 * Called when a mouse clicks on this object's collider
+	 */
+	void OnMouseDown()
+	{
+		// Tell manager that this gameObject was clicked
+		ColorManager.instance.SetCubeSelected(this.gameObject);
+	}
+}
